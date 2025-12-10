6284a66d84b20ff84e3e2a3ef422717b303ab159
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-10 21:58:10 +0600

    featt:add  condition for path

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,18 @@
+"use client"
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const Nav = () => {
+   const pathName =  usePathname()
   return (
     <div className='w-full h-20 bg-white  flex justify-between items-center px-5'>
         <div>🌍 Travel Guide</div>
         <div>
             <ul className='flex justify-between items-center gap-5'>
-                <Link href={"/"}><li>Home</li></Link> 
-                <Link href={"/about"}><li>Destination  </li></Link> 
-                <Link href={"/contact"}><li>Contact </li></Link>                               
+                <Link href={"/"} className={pathName=="/" ? "text-red-600" : ""} ><li>Home</li></Link> 
+                <Link href={"/destinations"}  className={pathName=="/destinations" ? "text-red-600" : ""}><li>Destinations  </li></Link> 
+                <Link href={"/contact"}  className={pathName=="/contact" ? "text-red-600" : ""}><li>Contact </li></Link>                               
             </ul>
         </div>
     </div>
