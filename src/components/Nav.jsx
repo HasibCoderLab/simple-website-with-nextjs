060e1a1c1d83f3e00a25fcdee9faa59d86681828
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-10 21:33:10 +0600

    featt:add simple nav style

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div className='w-full h-25 bg-white'>
+    <div className='w-full h-20 bg-white  flex justify-between items-center px-5'>
         <div>🌍 Travel Guide</div>
         <div>
-            <ul>
+            <ul className='flex justify-between items-center gap-5'>
                 <Link href={"/"}><li>Home</li></Link> 
                 <Link href={"/about"}><li>Destination  </li></Link> 
                 <Link href={"/contact"}><li>Contact </li></Link>                               
