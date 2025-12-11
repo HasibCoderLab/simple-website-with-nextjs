f8729c2446e60e7c9ffad1f8ddc038ae662534ef
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-11 16:08:40 +0600

    use params components

--- src/app/destinations/[city]/@info/page.jsx
+++ src/app/destinations/[city]/@info/page.jsx
@@ -1,9 +1,12 @@
+'use client'
+import { useParams } from 'next/navigation'
 import React from 'react'
 
 const page = () => {
+    const {city}  = useParams()
   return (
     <div  className='w-[50%] mt-[100px] text-white'>
-        This is Parallel Route 
+       {city}  is a very Beautiful
     </div>
   )
 }
