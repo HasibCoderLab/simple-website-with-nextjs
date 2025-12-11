c4cde5b26d2cfb80b6e3394c6c9f6b299cf11bce
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-11 13:02:24 +0600

    feat :use Array for run map

--- src/app/destinations/page.jsx
+++ src/app/destinations/page.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
 const page = () => {
+  const destination = ["Paris , Tokyo , NewWork"];
   return (
-    <div>page</div>
+    <div className=' h-full flex justify-center items-center text-white '>
+      <div className=' font-bold  text-2xl'>
+        Choose your Destinations
+      </div>
+      <div>
+        {
+          destination.map((d, index) => {
+            <div key={index}>
+              {d}
+            </div>
+          })
+        }
+      </div>
+    </div>
   )
 }
 
